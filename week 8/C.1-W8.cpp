5df84bf33c00584dc0f5a489bd2cdf5159f81a48
Phùng Đình Thành <78190218+DinhThanh-20021582@users.noreply.github.com>
2021-04-16 00:06:44 +0700

    Update C.1-W8.cpp

--- week 8/C.1-W8.cpp
+++ week 8/C.1-W8.cpp
@@ -1,49 +1,83 @@
 #include <iostream>
-
+#include <stdio.h>
 using namespace std;
 
 //a.Dao xau
 void reverse(char a[])
 {
-    char *GetChuoi=NULL;
-    int length = strlen(a);
-    int scan = 0;
- 
-    GetChuoi=(char*)malloc(sizeof(char));
- 
-    while (scan < length)
-    {
-        *(GetChuoi + scan) = a[length - scan++ - 1];
-    }  
-    *(GetChuoi + scan) = "\0"; 
- 
-    return GetChuoi;    
-}
-//b.Xoa ki tu
-void delete_char(char a[],char c)
-{
+    int count = 0;
+	while (a[count] != '\0') count++;
+    char * temp = new char[count + 1];
+    //char temp[count + 1];
+	for (int i = 0; i < count; i++)
+	{
+		 *(temp + i) = a[count - 1 - i];
+	}
+	temp[count] = '\0';
+	for(int i = 0; i < count; i++){
+		cout << *(temp + i);
+ }
 
+//b.Xoa ki tu
+void delete_char(char a[], char c)
+{
+	int count = 0;
+	while (a[count] != '\0') count++;
+	for(int i = 0; i < count; i++){
+		if(a[i] != c) cout << a[i];
+	}
 }
 //c.Don phai
-void pad_right(char a[],int n)
-{
-}
+// void pad_right(char * s, int n)
+// {
+// 	int count = 0;
+// 	while (*(s + count) != '\0') count++;
+// 	if (count < n) {for (int i = count; i < n; i++) *(s + i) = '_'; *(s + n) = '\0';}
+// 	for(int i = 0; i < n; i++){
+// 		cout << *(s + i);
+// 	}
+// }
 //d.Don trai
 void pad_left(char a[],int n)
 {
 }
 //e.Cat xau
-void truncate(char a[],int n)
-{
+void truncate(char a[], int n)
+{	
+	int count = 0;
+	while (a[count] != '\0') count++;
+	if(count > n) {for (int i = 0; i < n; i++) {
+		cout << a[i];
+		}
+	}
 }
 //f.Doi guong
-void bool_is_palidrome(char a[])
-{
-}
+// bool is_palindrome(char a[])
+// {
+// 	int count = 0;
+// 	while(a[count] != '\0') count++;
+// 	char * temp = new char[count + 1];
+// 	for(int i = 0; i < count; i++){
+// 		*(temp + i) = a[i];
+// 	}
+// 	temp[count] = '\0';
+// 	for(int i = 0; i < count; i++) {
+// 		for(int j = count - 1; i >= 0; i--){
+// 		if(a[i] != *(temp + j)) return false;
+// 		else return true;
+// 		}
+// 	}
+
 //g.Loc trai
 void trim_left(char a[])
 {
+	int count;
+	while(a[count] != '\0') count++;
+	for(int i = 0; i < count; i++){
+		if(a[i] != '_') cout << a[i];
+	}
 }
+
 //h.Loc phai
 void trim_right(char a[])
 {
